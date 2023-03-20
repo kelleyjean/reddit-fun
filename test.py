8e360417086abfe58ea3f8babcea43465e842165
kelleyjean <kelleyshann@gmail.com>
2023-03-19 20:45:08 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 print("hello")
 
-def print(hello):
+def printt_hello(hello):
     if hello = "hello":
         print("it fukin worked")
     else:
         print("it didn't fukin work")
+        
+print_hello()
